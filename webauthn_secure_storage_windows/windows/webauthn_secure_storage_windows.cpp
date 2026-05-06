5fb5a9fef32859ab36cae91b0e22829e6a2695dd
JohnGalt1717 <JohnGalt1717@users.noreply.github.com>
2026-05-06 15:03:52 -0400

    Optimize CI fan-out and fix Windows export

--- webauthn_secure_storage_windows/windows/webauthn_secure_storage_windows.cpp
+++ webauthn_secure_storage_windows/windows/webauthn_secure_storage_windows.cpp
@@ -1,17 +1,18 @@
+#include "include/webauthn_secure_storage_windows/webauthn_secure_storage_windows_plugin.h"
 #include "include/webauthn_secure_storage_windows/webauthn_secure_storage_windows_plugin_c_api.h"
 
 #include <flutter/plugin_registrar_windows.h>
 
 #include "webauthn_secure_storage_windows_plugin.h"
 
-void WebauthnSecureStorageWindowsPluginRegisterWithRegistrar(
-        FlutterDesktopPluginRegistrarRef registrar) {
-    webauthn_secure_storage_windows::WebauthnSecureStorageWindowsPlugin::RegisterWithRegistrar(
-            flutter::PluginRegistrarManager::GetInstance()
-                    ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+extern "C" void WebauthnSecureStorageWindowsPluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+  webauthn_secure_storage_windows::WebauthnSecureStorageWindowsPlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarManager::GetInstance()
+          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
 }
 
-void WebauthnSecureStorageWindowsPluginCApiRegisterWithRegistrar(
+extern "C" void WebauthnSecureStorageWindowsPluginCApiRegisterWithRegistrar(
     FlutterDesktopPluginRegistrarRef registrar) {
   webauthn_secure_storage_windows::WebauthnSecureStorageWindowsPlugin::RegisterWithRegistrar(
       flutter::PluginRegistrarManager::GetInstance()
